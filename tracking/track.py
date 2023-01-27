28ca778ca2de64bd8029744022f234f8720700aa
sirius <690679581@qq.com>
2023-01-27 13:32:27 +0800

    1.27

--- tracking/track.py
+++ tracking/track.py
@@ -4,18 +4,31 @@ import pickle
 import static
 from TrackUtils import *
 
-def iterate(uid,location,map):
-    BTConnection = findBTConnection(uid,location,map)
-    print(len(BTConnection))
-    selectedUser = np.random.choice(BTConnection,size=static.userPerIter,replace=False)
-    print()
 
-def findBTConnection(uid,location,map):
+def iterate(uid, location, map, creditRate, depth,time):
+    if depth == 4:
+        return creditRate
+    depth += 1
+    BTConnection = findBTConnection(uid, location, map)
+    userNum = len(BTConnection)
+    creditRate = 0
+    selectedUser = []
+    if userNum < static.userPerIter:
+        creditRate = userNum / static.userPerIter
+        selectedUser = BTConnection
+    else:
+        creditRate = 1
+        selectedUser = np.random.choice(BTConnection, size=static.userPerIter, replace=False)
+    for u in selectedUser:
+        readObj("../mapAnalog/map/map{}".format(time))
+
+
+def findBTConnection(uid, location, map):
     BTConnectionCircle = findCircle(int(static.bluetoothDistance / 10), location)
     BTConnection = []
     for point in BTConnectionCircle:
         BTConnection.extend(map[point[0]][point[1]])
-    BTConnection.remove(uid)    #去掉自己
+    BTConnection.remove(uid)  # 去掉自己
     return BTConnection
 
 
@@ -23,8 +36,7 @@ if __name__ == '__main__':
     record = readObj("../mapAnalog/map/map0.pkl")
     map = record.map
     userList = record.userList
-    # startTime = 50
+    startTime = 50
     # uid = 50
 
-    iterate(5,userList[4].location,map)
-
+    iterate(5, userList[4].location, map, 1, 1,time=startTime)
