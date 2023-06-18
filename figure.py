082e302fbdb8b3d1f35b9bf33303bc40b7bda016
Sirius <690679581@qq.com>
2023-06-18 19:50:30 +0800

    6.18

--- figure.py
+++ figure.py
@@ -4,6 +4,24 @@ import numpy as np
 
 matplotlib.rcParams['font.sans-serif'] = ['Arial']
 
+fontTitle = {
+    'family': 'Arial',
+    'weight': 'normal',
+    # 'style': 'italic',
+    'size': 18,
+}
+fontLabel = {
+    'family': 'Arial',
+    'weight': 'bold',
+    # 'style': 'italic',
+    'size': 15,
+}
+fontTick = {
+    'family': 'Arial',
+    'weight': 'normal',
+    'style': 'italic',
+    'size': 12,
+}
 if __name__ == '__main__':
     basedir = "tracking/Res/density-"
     means = {"3": [], "4": [], "5": [], "6": [], "7": []}
@@ -24,18 +42,45 @@ if __name__ == '__main__':
         var[str(wn)] = [np.round(np.var(i), 3) for i in [res10000, res25000, res50000, res75000, res100000, res200000]]
         allPoints[str(wn)] = [np.round(i, 3) for i in [res10000, res25000, res50000, res75000, res100000, res200000]]
     x = np.array([100, 250, 500, 750, 1000, 2000])
-    y = np.array([0.0,0.2,0.4,0.6,0.8,1.0])
+    y = np.array([0.2,0.4,0.6,0.8,1.0])
 
     plt.figure(dpi=300)
-    # subplot(151)，已废弃
-    # ax1 = plt.subplot(221)
-    # ax2 = plt.subplot(222)
-    # ax3 = plt.subplot(223)
-    # ax4 = plt.subplot(224)
-    # ax5 = plt.subplot(155)
 
     # 平均值折线图
-    ax = plt.subplot(111)
+    # ax = plt.subplot(111)
+    # colors = ['#264653', '#2a9d8e', '#e9c46b','#f3a261','#e66f51']
+    # for (k,v),c in zip(means.items(),colors):
+    #     #拟合曲线图
+    #     # f = np.polyfit(x, v, 3)
+    #     # x2 = np.linspace(0, 2000, 2001, endpoint=True)
+    #     # p = np.poly1d(f)
+    #     # y2 = p(x2)
+    #     # ax.plot(x2,y2,label="Witness = "+k,linewidth=2)
+    #
+    #     #折线图
+    #     ax.plot(x,v,label="Witness = "+k,linewidth=2,color = c)
+    # ax.set_xlabel("Population Density (persons/km2)",fontLabel)
+    # ax.set_ylabel("Credibility",fontLabel)
+    # ax.set_xscale("log")
+    # # ax.set_yscale("log")
+    # ax.set_xticks(x)
+    # ax.set_xticklabels(x, fontTick)
+    # ax.set_yticks(y)
+    # ax.set_yticklabels(y, fontTick)
+    # ax.legend(loc="lower right",prop={"size":13})
+    # plt.savefig("fig/lineChart.png",dpi=300)
+    # plt.show()
+
+    # 总图
+    plt.figure(figsize=(15,10))
+    ax = plt.subplot(2,3,1)
+    ax1 = plt.subplot(2,3,2)
+    ax2 = plt.subplot(2,3,3)
+    ax3 = plt.subplot(2,3,4)
+    ax4 = plt.subplot(2,3,5)
+    ax5 = plt.subplot(2,3,6)
+
+    #折线图
     colors = ['#264653', '#2a9d8e', '#e9c46b','#f3a261','#e66f51']
     for (k,v),c in zip(means.items(),colors):
         #拟合曲线图
@@ -47,64 +92,51 @@ if __name__ == '__main__':
 
         #折线图
         ax.plot(x,v,label="Witness = "+k,linewidth=2,color = c)
-    ax.set_xlabel("Population Density (persons/km2)")
-    ax.set_ylabel("Credibility")
+    ax.set_xlabel("Population Density (persons/km2)",fontLabel)
+    ax.set_ylabel("Credibility",fontLabel)
     ax.set_xscale("log")
-    ax.set_yscale("log")
-    ax.legend(loc="lower right",prop={"size":13})
-    plt.show()
+    # ax.set_yscale("log")
+    ax.set_xticks(x)
+    ax.set_xticklabels(x, fontTick)
+    ax.set_yticks(y)
+    ax.set_yticklabels(y, fontTick)
+    ax.legend(loc="lower right", prop={"size": 13})
+    ax.set_title()
 
-    # 箱线图
-    # ax1 = plt.subplot(3,2,1)
-    # ax2 = plt.subplot(3,2,2)
-    # ax3 = plt.subplot(3,2,3)
-    # ax4 = plt.subplot(3,2,4)
-    # ax5 = plt.subplot(3,2,5)
-    # axes = [ax1,ax2,ax3,ax4,ax5]
-    # for ax,WN in zip(axes,range(3,8)):
-    #     WN = str(WN)
-    #     bp = ax.boxplot(allPoints[WN],
-    #                     labels=x,
-    #                     vert=True,
-    #                     patch_artist=True,
-    #                     showmeans=True,
-    #                     meanline=False,
-    #                     flierprops=dict(marker='o', markerfacecolor='black'))
-    #     ax.plot(range(1, 7), means[WN], label="Mean", linewidth=2)
-    #     ax.plot(range(1, 7), medians[WN], label="Median", linewidth=2)
-    #     # 颜色填充
-    #     colors = ['#4eab90A0', '#8eb69cA0', '#edddc3A0', '#eebf6dA0', '#d94f33A0', '#834026A0']
-    #     for patch, color in zip(bp['boxes'], colors):
-    #         patch.set_facecolor(color)
-    #     fontTitle = {
-    #         'family': 'Arial',
-    #         'weight': 'normal',
-    #         # 'style': 'italic',
-    #         'size': 18,
-    #     }
-    #     fontLabel = {
-    #         'family': 'Arial',
-    #         'weight': 'bold',
-    #         # 'style': 'italic',
-    #         'size': 15,
-    #     }
-    #     fontTick = {
-    #         'family': 'Arial',
-    #         'weight': 'normal',
-    #         'style': 'italic',
-    #         'size': 12,
-    #     }
-    #     ax.set_xlabel("Population Density (persons/km²)", fontLabel)
-    #     ax.set_ylabel("Credibility", fontLabel)
-    #     ax.set_xticklabels(x, fontTick)
-    #     ax.set_yticks(y)
-    #     ax.set_yticklabels(y,fontTick)
-    #     # ax.yaxis.grid(True)
-    #     ax.spines["top"].set_linewidth(1.5)
-    #     ax.spines["bottom"].set_linewidth(1.5)
-    #     ax.spines["left"].set_linewidth(1.5)
-    #     ax.spines["right"].set_linewidth(1.5)
-    #     # ax.legend(loc="lower right", prop={"size": 13})
-    #     ax.set_title("Witness = " + WN,fontdict=fontTitle)
+    #箱线图
+    axes = [ax1,ax2,ax3,ax4,ax5]
+    for ax,WN in zip(axes,range(3,8)):
+        WN = str(WN)
+        bp = ax.boxplot(allPoints[WN],
+                        labels=x,
+                        vert=True,
+                        patch_artist=True,
+                        showmeans=True,
+                        meanline=True,
+                        zorder=1,
+                        flierprops=dict(marker='o', markerfacecolor='black'),
+                        medianprops={"linewidth":1.5},
+                        meanprops={"linewidth":1.5,"color":"#1f77b4"})
+        ax.plot(range(1, 7), means[WN], label="Mean", linewidth=2,zorder=2)
+        ax.plot(range(1, 7), medians[WN], label="Median", linewidth=2,zorder=2)
+        # 颜色填充
+        boxcolors = ['#4eab90A0', '#8eb69cA0', '#edddc3A0', '#eebf6dA0', '#d94f33A0', '#834026A0']
+        colors = ['#4eab90', '#8eb69c', '#edddc3', '#eebf6d', '#d94f33', '#834026']
+        for patch, boxcolor,color in zip(bp['boxes'], boxcolors,colors):
+            # patch.set_color(color)
+            patch.set_facecolor(boxcolor)
+
+        ax.set_xlabel("Population Density (persons/km²)", fontLabel)
+        ax.set_ylabel("Credibility", fontLabel)
+        ax.set_xticklabels(x, fontTick)
+        ax.set_yticks(y)
+        ax.set_yticklabels(y,fontTick)
+        ax.spines["top"].set_linewidth(1.5)
+        ax.spines["bottom"].set_linewidth(1.5)
+        ax.spines["left"].set_linewidth(1.5)
+        ax.spines["right"].set_linewidth(1.5)
+        ax.legend(loc="lower right", prop={"size": 13})
+        ax.set_title("Witness = " + WN,fontdict=fontTitle)
     plt.tight_layout()
+    plt.savefig("fig/boxChart.png",dpi=300)
     plt.show()
--- result.py
+++ result.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
     data = {}
     for th in [0.4, 0.6, 0.8, 1.0]:
         basedir = "tracking/Credit/threshold-" + str(th) + "/"
-        t0990 = np.load(basedir + "th-0.8_credit0.99-0.01.npy")
+        t0990 = np.load(basedir + "credit0.99-0.01.npy")
         t0975 = np.load(basedir + "credit0.975-0.025.npy")
         t0950 = np.load(basedir + "credit0.95-0.05.npy")
         t0900 = np.load(basedir + "credit0.9-0.1.npy")
@@ -96,90 +96,91 @@ if __name__ == '__main__':
     misjugListSum = np.array(misjugListSum)
 
     # 折线图
-    # for ax, title, data in zip(axes, ["Accuracy", "Error Detection Rate", "Error Missing Rate", "Misjudgement Rate"],
-    #                            [accListSum, errdetListSum, errmisListSum, misjugListSum]):
-    #     for line,index in zip(data,["1.0","0.8","0.6","0.4"]):
-    #         ax.plot(x, line, linewidth=2,label="Threshold = "+index)
-    #
-    #     fontTitle = {
-    #         'family': 'Arial',
-    #         'weight': 'normal',
-    #         # 'style': 'italic',
-    #         'size': 15,
-    #     }
-    #     fontLabel = {
-    #         'family': 'Arial',
-    #         'weight': 'bold',
-    #         # 'style': 'italic',
-    #         'size': 13,
-    #     }
-    #     fontTick = {
-    #         'family': 'Arial',
-    #         'weight': 'normal',
-    #         'style': 'italic',
-    #         'size': 11,
-    #     }
-    #     ax.set_xlabel("Percentage of Cheaters", fontLabel)
-    #     ax.set_ylabel("Value", fontLabel)
-    #     ax.set_xscale("log")
-    #     # ax.set_ylim(0.0,1.0)
-    #     ax.set_xticks(x)
-    #     ax.set_xticklabels(x, fontTick)
-    #     ax.set_yticks(y)
-    #     ax.set_yticklabels(y, fontTick)
-    #     # ax.yaxis.grid(True)
-    #     ax.spines["top"].set_linewidth(1.5)
-    #     ax.spines["bottom"].set_linewidth(1.5)
-    #     ax.spines["left"].set_linewidth(1.5)
-    #     ax.spines["right"].set_linewidth(1.5)
-    #     ax.set_title(title, fontdict=fontTitle)
-    # plt.tight_layout()
-    # plt.show()
+    for ax, title, data in zip(axes, ["Accuracy", "Error Detection Rate", "Error Missing Rate", "Misjudgement Rate"],
+                               [accListSum, errdetListSum, errmisListSum, misjugListSum]):
+        for line,index in zip(data,["1.0","0.8","0.6","0.4"]):
+            ax.plot(x, line, linewidth=2,label="Threshold = "+index)
+
+        fontTitle = {
+            'family': 'Arial',
+            'weight': 'normal',
+            # 'style': 'italic',
+            'size': 15,
+        }
+        fontLabel = {
+            'family': 'Arial',
+            'weight': 'bold',
+            # 'style': 'italic',
+            'size': 13,
+        }
+        fontTick = {
+            'family': 'Arial',
+            'weight': 'normal',
+            'style': 'italic',
+            'size': 11,
+        }
+        ax.set_xlabel("Percentage of Cheaters", fontLabel)
+        ax.set_ylabel("Value", fontLabel)
+        ax.set_xscale("log")
+        # ax.set_ylim(0.0,1.0)
+        ax.set_xticks(x)
+        ax.set_xticklabels(x, fontTick)
+        ax.set_yticks(y)
+        ax.set_yticklabels(y, fontTick)
+        # ax.yaxis.grid(True)
+        ax.spines["top"].set_linewidth(1.5)
+        ax.spines["bottom"].set_linewidth(1.5)
+        ax.spines["left"].set_linewidth(1.5)
+        ax.spines["right"].set_linewidth(1.5)
+        ax.set_title(title, fontdict=fontTitle)
+        ax.legend()
+    plt.tight_layout()
+    plt.show()
 
 
     # 柱状图
-    fontTitle = {
-        'family': 'Arial',
-        'weight': 'normal',
-        # 'style': 'italic',
-        'size': 15,
-    }
-    fontLabel = {
-        'family': 'Arial',
-        'weight': 'bold',
-        # 'style': 'italic',
-        'size': 13,
-    }
-    fontTick = {
-        'family': 'Arial',
-        'weight': 'normal',
-        'style': 'italic',
-        'size': 11,
-    }
-    # colors = ["#264653","#2a9d8c","#e9c46b","#e66f51"]
-    colors = ["#a40545","#f48f44","#fdd985","#7fcba4"]
-    for ax, (th, res) in zip(axes, data.items()):
-        ax.set_title("Threshold = " + th[2:], fontdict=fontTitle)
-        TT = np.array([i["TT"] for i in res.values()])
-        TF = np.array([i["TF"] for i in res.values()])
-        FT = np.array([i["FT"] for i in res.values()])
-        FF = np.array([i["FF"] for i in res.values()])
-        ax.bar(range(1, len(x) + 1), FF, width=0.6, color=colors[0], label="FF")
-        ax.bar(range(1, len(x) + 1), TF, width=0.6, color=colors[1], bottom=FF, label="TF")
-        ax.bar(range(1, len(x) + 1), FT, width=0.6, color=colors[2], bottom=FF + TF, label="FT")
-        ax.bar(range(1, len(x) + 1), TT, width=0.6, color=colors[3], bottom=FF + TF + FT, label="TT")
-        # ax.bar(range(1, len(x) + 1), FF, width=0.6, color=colors[0], edgecolor="black", linewidth=0.5, label="FF")
-        # ax.bar(range(1, len(x) + 1), TF, width=0.6, color=colors[1], edgecolor="black", linewidth=0.5, bottom=FF, label="TF")
-        # ax.bar(range(1, len(x) + 1), FT, width=0.6, color=colors[2], edgecolor="black", linewidth=0.5, bottom=FF + TF, label="FT")
-        # ax.bar(range(1, len(x) + 1), TT, width=0.6, color=colors[3], edgecolor="black", linewidth=0.5, bottom=FF + TF + FT, label="TT")
-        ax.set_xticks(range(1, len(x) + 1))
-        ax.set_xticklabels(x, fontTick)
-        ax.set_ylim(0, 1000)
-        ax.set_xlabel("Percentage of Cheaters", fontLabel)
-        ax.set_ylabel("Persons", fontLabel)
-
-    # plt.legend(ncol=4,bbox_to_anchor=(0.5, 0),prop=fontLabel)
-    # plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.2, hspace=0.5)
-    # plt.figure(figsize=(10,10))
-    plt.tight_layout()
-    plt.show()
+    # fontTitle = {
+    #     'family': 'Arial',
+    #     'weight': 'normal',
+    #     # 'style': 'italic',
+    #     'size': 15,
+    # }
+    # fontLabel = {
+    #     'family': 'Arial',
+    #     'weight': 'bold',
+    #     # 'style': 'italic',
+    #     'size': 13,
+    # }
+    # fontTick = {
+    #     'family': 'Arial',
+    #     'weight': 'normal',
+    #     'style': 'italic',
+    #     'size': 11,
+    # }
+    # # colors = ["#264653","#2a9d8c","#e9c46b","#e66f51"]
+    # colors = ["#a40545","#f48f44","#fdd985","#7fcba4"]
+    # for ax, (th, res) in zip(axes, data.items()):
+    #     ax.set_title("Threshold = " + th[2:], fontdict=fontTitle)
+    #     TT = np.array([i["TT"] for i in res.values()])
+    #     TF = np.array([i["TF"] for i in res.values()])
+    #     FT = np.array([i["FT"] for i in res.values()])
+    #     FF = np.array([i["FF"] for i in res.values()])
+    #     ax.bar(range(1, len(x) + 1), FF, width=0.6, color=colors[0], label="FF")
+    #     ax.bar(range(1, len(x) + 1), TF, width=0.6, color=colors[1], bottom=FF, label="TF")
+    #     ax.bar(range(1, len(x) + 1), FT, width=0.6, color=colors[2], bottom=FF + TF, label="FT")
+    #     ax.bar(range(1, len(x) + 1), TT, width=0.6, color=colors[3], bottom=FF + TF + FT, label="TT")
+    #     # ax.bar(range(1, len(x) + 1), FF, width=0.6, color=colors[0], edgecolor="black", linewidth=0.5, label="FF")
+    #     # ax.bar(range(1, len(x) + 1), TF, width=0.6, color=colors[1], edgecolor="black", linewidth=0.5, bottom=FF, label="TF")
+    #     # ax.bar(range(1, len(x) + 1), FT, width=0.6, color=colors[2], edgecolor="black", linewidth=0.5, bottom=FF + TF, label="FT")
+    #     # ax.bar(range(1, len(x) + 1), TT, width=0.6, color=colors[3], edgecolor="black", linewidth=0.5, bottom=FF + TF + FT, label="TT")
+    #     ax.set_xticks(range(1, len(x) + 1))
+    #     ax.set_xticklabels(x, fontTick)
+    #     ax.set_ylim(0, 1000)
+    #     ax.set_xlabel("Percentage of Cheaters", fontLabel)
+    #     ax.set_ylabel("Persons", fontLabel)
+    #
+    # # plt.legend(ncol=4,bbox_to_anchor=(0.5, 0),prop=fontLabel)
+    # # plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.2, hspace=0.5)
+    # # plt.figure(figsize=(10,10))
+    # plt.tight_layout()
+    # plt.show()
